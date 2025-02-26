15f3f54e16a35865949f1dc637633148d97e62c1
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-02-26 13:08:08 +1100

    Just fooling around

---
+++ Glazing-Calc.cs
@@ -0,0 +1,24 @@
+using System;
+
+class GlazerCalc
+{
+	static void Main()
+	{
+		double width, height, woodLength, glassArea;
+		string widthString, heightString;
+
+		Console.WriteLine("Enter the width of the window:");
+		widthString = Console.ReadLine();
+		width = double.Parse(widthString);
+
+		Console.WriteLine("Enter the height of the window:");
+		heightString = Console.ReadLine();
+		height = double.Parse(heightString);
+
+		woodLength = 2 * (width + height) * 3.25;
+		glassArea = 2 * (width * height);
+
+		Console.WriteLine("The length of the wood is " + woodLength + " feet");
+		Console.WriteLine("The area of the glass is " + glassArea + " square meters");
+	}
+}
